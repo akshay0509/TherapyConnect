7717e7ace0467cc6e53853d9374a6f92bf45c754
akshay0509 <akshaynataraj@gmail.com>
2026-01-13 22:34:01 +0530

    logger changes

--- DiscoveryService/src/main/java/com/org/discoveryService/DiscoveryServiceApplication.java
+++ DiscoveryService/src/main/java/com/org/discoveryService/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.org.discoveryservice;
+package com.org.discoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
